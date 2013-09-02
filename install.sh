92251d46e0f8bd046d0f7eac554ea7d40cbbbe58
arik181 <arik181@gmail.com>
2013-09-01 17:34:52 -0700

    Added ranger config to repository.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+ln    ~/configfiles/.tmux.conf    ~/.tmux.conf
+ln    ~/configfiles/.vimrc        ~/.vimrc
+ln -s ~/configfiles/.xmonad       ~/.xmonad
+ln -s ~/configfiles/ranger        ~/.config/ranger
+ln    ~/configfiles/.zshfunc      ~/.zshfunc
+ln    ~/configfiles/.zshrc.Linux  ~/.zshrc
+ln    ~/configfiles/.xinitrc      ~/.xinitrc
+ln    ~/configfiles/.xprofile     ~/.xprofile
+ln    ~/configfiles/.xsession     ~/.xsession
