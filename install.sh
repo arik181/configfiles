66942da9a0c682d308a33897166555f460828a2f
Devin Quirozoliver <devin@mediamechanic.net>
2013-09-13 08:35:18 -0700

    Minor tweaks.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 ln    ~/Git/configfiles/.tmux.conf    ~/.tmux.conf
 ln    ~/Git/configfiles/.vimrc        ~/.vimrc
+rm -rf ~/.xmonad
 ln -s ~/Git/configfiles/.xmonad       ~/.xmonad
+rm -rf ~/.config/ranger
 ln -s ~/Git/configfiles/ranger        ~/.config/ranger
 ln    ~/Git/configfiles/.zshfunc      ~/.zshfunc
 ln    ~/Git/configfiles/.zshrc.Linux  ~/.zshrc
