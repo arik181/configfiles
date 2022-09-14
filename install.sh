174cff06d2601b27fbd56280add5c66b3848cd2a
arik181 <arik181@gmail.com>
2022-09-14 16:21:50 -0700

    tweak install

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 rm -rf ~/.tmux.conf
 rm -rf ~/.vimrc
 rm -rf ~/.xmonad
-rm -rf ~/.config/ranger
 rm -rf ~/.zshfunc
 rm -rf ~/.zshrc
 rm -rf ~/.xinitrc
 rm -rf ~/.xprofile
 rm -rf ~/.xsession
-rm -rf ~/.config/sway/config
+rm -rf ~/.config/sway
+
+mkdir ~/.config/sway
 
 ln -s ~/Git/configfiles/.tmux.conf    ~/.tmux.conf
 ln -s ~/Git/configfiles/.vimrc        ~/.vimrc
 ln -s ~/Git/configfiles/.xmonad       ~/.xmonad
-ln -s ~/Git/configfiles/ranger        ~/.config/ranger
 ln -s ~/Git/configfiles/.zshfunc      ~/.zshfunc
 ln -s ~/Git/configfiles/.zshrc.Linux  ~/.zshrc
 ln -s ~/Git/configfiles/.xinitrc      ~/.xinitrc
